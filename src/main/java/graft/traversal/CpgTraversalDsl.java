ba60939ae86d31fccf6ce7379bd0c704f78a6ec4
Wim Keirsgieter <wim.keirsgieter@gmail.com>
2019-08-01 14:57:17 +0200

    Implement reassigns, sanitizes and sunkArgs as DSL traversals

--- src/main/java/graft/traversal/CpgTraversalDsl.java
+++ src/main/java/graft/traversal/CpgTraversalDsl.java
@@ -1,9 +1,15 @@
 package graft.traversal;
 
+import java.util.List;
+
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.GremlinDsl;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
 import org.apache.tinkerpop.gremlin.structure.Edge;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
+
+import graft.analysis.taint.SanitizerDescription;
+import graft.analysis.taint.SinkDescription;
+import graft.utils.GraphUtil;
 
 import static graft.Const.*;
 
@@ -15,18 +21,84 @@ import static graft.Const.*;
 @GremlinDsl(traversalSource = "graft.traversal.CpgTraversalSourceDsl")
 public interface CpgTraversalDsl<S, E> extends GraphTraversal.Admin<S, E> {
 
+//    /**
+//     * Fills the given list with all invoke expression nodes found in the AST subtree of the given node.
+//     *
+//     * Note that any existing values in the list will not be modified.
+//     *
+//     * @param invokeExprs the list to add the invoke expression nodes to
+//     * @return the identity traversal
+//     */
+//    default GraphTraversal<S, ?> getInvokeExprs(List<Vertex> invokeExprs) {
+//        CpgTraversal clone = (CpgTraversal) this.clone();
+//        clone.repeat(sideEffect(it -> {
+//            Vertex vertex = (Vertex) it.get();
+//            if (vertex.value(NODE_TYPE).equals(INVOKE_EXPR)) {
+//                invokeExprs.add(vertex);
+//            }
+//        }).out(AST_EDGE)).iterate();
+//        return this;
+//    }
+
     /**
-     * Returns the ith parameter node of a method entry vertex.
+     * Checks whether the current node is a reassignment of the given local variable.
      *
-     * @param i the index of the parameter
-     * @return the ith parameter
+     * @param varName the name of the local variable to check
+     * @return a filter traversal that returns true if the node is a reassignment, otherwise false
      */
-    default GraphTraversal<S, Vertex> ithParam(int i) {
-        return hasLabel(CFG_NODE).has(NODE_TYPE, ENTRY)
-                .outE(AST_EDGE)
-                .has(EDGE_TYPE, PARAM)
-                .has(INDEX, i)
-                .inV();
+    default GraphTraversal<S, ?> reassigns(String varName) {
+        CpgTraversalSource g = GraphUtil.graph().traversal(CpgTraversalSource.class);
+        return filter(it -> {
+            Vertex vertex  = (Vertex) it.get();
+            CpgTraversal targetName = g.V(vertex)
+                    .outE(AST_EDGE)
+                    .has(EDGE_TYPE, TARGET)
+                    .inV()
+                    .values(NAME);
+            if (targetName.hasNext() && varName.equals(targetName.next())) {
+                return true;
+            }
+            return false;
+        });
+    }
+
+    /**
+     * Checks whether the current node sanitizes the given local variable, according to the given sanitizer descriptions.
+     *
+     * @param sanitizers the sanitizer descriptions
+     * @param varName the variable to check for sanitization
+     * @return a filter traversal that returns true if the node sanitizes the variable, otherwise false
+     */
+    default GraphTraversal<S, ?> sanitizes(List<SanitizerDescription> sanitizers, String varName) {
+        return filter(it -> {
+            for (SanitizerDescription sanitizer : sanitizers) {
+                if (sanitizer.sanitizes((Vertex) it.get(), varName)) {
+                    return true;
+                }
+            }
+            return false;
+        });
+    }
+
+    /**
+     * Returns a traversal containing all argument nodes that are "sunk" in the current sink node, according to the given
+     * sink description.
+     *
+     * The sink description specifies which argument indices should be considered sunk. If none are specified, then all
+     * arguments are considered sunk.
+     *
+     * @param sink
+     * @return
+     */
+    default GraphTraversal<S, Vertex> sunkArgs(SinkDescription sink) {
+        return outE(AST_EDGE)
+                .has(EDGE_TYPE, ARG)
+                .filter(it -> {
+                    if (sink.sinksArgs.size() == 0) return true;
+                    Edge argEdge = it.get();
+                    int index = argEdge.value(INDEX);
+                    return sink.sinksArgs.contains(index);
+                }).inV();
     }
 
     /**
