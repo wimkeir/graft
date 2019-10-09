4a9a62ad3c015f4a5ae300922b0de89c407c1607
Wim Keirsgieter <wim.keirsgieter@gmail.com>
2019-10-09 14:26:57 +0200

    Some more DSL tests

--- src/test/java/graft/traversal/TestCpgTraversalDsl.java
+++ src/test/java/graft/traversal/TestCpgTraversalDsl.java
@@ -1,5 +1,43 @@
 package graft.traversal;
 
+import graft.cpg.structure.CodePropertyGraph;
+import graft.cpg.structure.VertexDescription;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static graft.Const.*;
+import static org.junit.Assert.*;
+
 public class TestCpgTraversalDsl {
+
+    private static final String CPG_PATH = "src/test/resources/simple_cpg.json";
+    private static CodePropertyGraph cpg;
+
+    @BeforeClass
+    public static void setUpClass() {
+        cpg = CodePropertyGraph.fromFile(CPG_PATH);
+    }
+
+    @Test
+    public void testMatches() {
+        String regex = "[a-zA-Z_]*";
+        Map<String, String> props = new HashMap<>();
+        props.put(NODE_TYPE, ENTRY);
+        props.put(TEXT_LABEL, regex);
+        VertexDescription descr = new VertexDescription("entry-nodes", CFG_NODE, props);
+
+        List<Vertex> matches = cpg.traversal().V().matches(descr).toList();
+        assertEquals(4, matches.size());
+
+        for (Vertex match : matches) {
+            assertEquals(ENTRY, match.value(NODE_TYPE));
+            assertTrue(match.value(TEXT_LABEL).toString().matches(regex));
+        }
+    }
 
 }
