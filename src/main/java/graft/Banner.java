d9fe6566221f37c3987ae5f63633378541372164
Wim Keirsgieter <wim.keirsgieter@gmail.com>
2019-10-31 09:32:45 +0200

    Cooler banner display

--- src/main/java/graft/Banner.java
+++ src/main/java/graft/Banner.java
@@ -2,19 +2,42 @@ package graft;
 
 public class Banner {
 
-    private static final int DEFAULT_WIDTH = 100;
+    private static int DEFAULT_WIDTH = 100;
 
-    private StringBuilder sb = new StringBuilder();
-    private int width;
+    private StringBuilder sb;
+    private int width = 100;
+    private String heading;
 
     public Banner() {
         this(DEFAULT_WIDTH);
     }
 
     public Banner(int width) {
+        this(width, "");
+    }
+
+    public Banner(String heading) {
+        this(DEFAULT_WIDTH, heading);
+    }
+
+    public Banner(int width, String heading) {
         this.width = width;
-        for (int i = 0; i < width; i++) sb.append('=');
-        sb.append(String.format("\n| %1$-" + (width - 4) + "s |\n", " "));
+        this.heading = heading;
+        sb = new StringBuilder();
+
+        border();
+        println();
+
+        int lenHeading = heading.length();
+        if (lenHeading > 0) {
+            println(heading);
+            println();
+        }
+    }
+
+
+    public void println() {
+        sb.append(String.format("| %1$-" + (width - 4) + "s |\n", " "));
     }
 
     public void println(String s) {
@@ -22,9 +45,16 @@ public class Banner {
     }
 
     public void display() {
-        sb.append(String.format("| %1$-" + (width - 4) + "s |\n", " "));
-        for (int i = 0; i < width; i++) sb.append('=');
+        println();
+        border();
         System.out.println(sb.toString());
+    }
+
+    private void border() {
+        for (int i = 0; i < width; i++) {
+            sb.append('=');
+        }
+        sb.append('\n');
     }
 
 }
